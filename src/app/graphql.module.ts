95840f1e227db2c7e482e692797541099d1d89e8
Nrinconv <nrinconv@unal.edu.co>
2023-06-10 23:50:13 -0500

    ip ag changed 4

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -8,7 +8,7 @@ import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 
 
 //const uri = 'https://swapi-graphql.netlify.app/.netlify/functions/index';
-const uri = 'http://twiddit-ag-svc.default.svc.cluster.local:5000/graphql'; // <-- add the URL of the GraphQL server here
+const uri = 'http://34.121.24.182:5000/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({ uri }),
